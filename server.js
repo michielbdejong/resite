3fab6b725e8fb4587940964f2ab98300c66f38c2
Michiel de Jong <michiel@unhosted.org>
2014-02-14 07:35:47 +0000

    add index.html func, prepare for tls

--- server.js
+++ server.js
@@ -2,18 +2,23 @@ process.umask(077);
 process.env.DEBUG = true;
 
 var http = require('http'),
+    https = require('https'),
     reStore = require('./vendor/restore'),
     store   = new reStore.FileTree({path: '../restore/storage'}),
     userName = 'michiel',
-    sitepath = '/public/www/michielbdejong.com';
+    sitepath = '/public/www/michielbdejong.com',
+    certpath = '/tls/michielbdejong.com';
 
 //store.putItem(userName, 'content:/public/www/michielbdejong.com/test', 'It worked!', function(err) {
 //  console.log(err);
 //});
 
 http.createServer(function(req, res) {
- console.log('store.getItem', userName, 'content:'+sitepath+ req.url);
- store.getItem(userName, 'content:'+sitepath+ req.url, function(err, content) {
+  if (req.url.substr(-1) === '/') {
+    req.url += 'index.html';
+  }
+  console.log('store.getItem', userName, 'content:'+sitepath+ req.url);
+  store.getItem(userName, 'content:'+sitepath+ req.url, function(err, content) {
     res.end(content);
   });
 }).listen(80);
